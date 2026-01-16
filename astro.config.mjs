3bf41aa2c0cc3d112973e1da70372db065eaecaf
domewo <domewo227@gmail.com>
2026-01-16 18:21:28 +0900

    Fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/domewo/site',
+  site: 'https://domewo.github.io',
+  base: '/site',
   integrations: [tailwind()],
 });
