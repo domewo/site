bedc665cf558faebce4342ff57d9d5e8766c62fc
domewo <domewo227@gmail.com>
2026-01-16 17:35:00 +0900

    First deploy

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://domewo.github.io/site/',
+  integrations: [tailwind()],
+});
